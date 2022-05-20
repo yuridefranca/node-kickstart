4db3edf731f2025fd6ce6aba6d599d3be03a79aa
Yuri Serafim <yuri.serafim@okn.com.br>
2022-05-20 15:58:37 -0300

    WIP Adicionado menu de kickstart

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
-# Introduction 
-TODO: Give a short introduction of your project. Let this section explain the objectives or the motivation behind this project. 
+# Node Kickstart
 
-# Getting Started
-TODO: Guide users through getting your code up and running on their own system. In this section you can talk about:
-1.	Installation process
-2.	Software dependencies
-3.	Latest releases
-4.	API references
+### Repositório com o kickstart para projetos em NodeJs <br><br>
 
-# Build and Test
-TODO: Describe and show how to build your code and run the tests. 
+### Pré-requisitos <br>
 
-# Contribute
-TODO: Explain how other users and developers can contribute to make your code better. 
+Para fazer o uso desse kickstart você precisará ter em seu computador as seguintes ferramentas: <br>
+    > Git - https://git-scm.com/  <br>
+    > Docker - https://www.docker.com/  <br><br>
 
-If you want to learn more about creating good readme files then refer the following [guidelines](https://docs.microsoft.com/en-us/azure/devops/repos/git/create-a-readme?view=azure-devops). You can also seek inspiration from the below readme files:
-- [ASP.NET Core](https://github.com/aspnet/Home)
-- [Visual Studio Code](https://github.com/Microsoft/vscode)
-- [Chakra Core](https://github.com/Microsoft/ChakraCore)
+### Modo de usar
+``` bash
+# Clone o repositório
+$ git@ssh.dev.azure.com:v3/okndevelopers/okn-labs/node-kickstart
+
+# Execute o seguinte comando na raiz do seu projeto
+$ source init.sh
+
+# Abrirá um menu para onde será definido as configurações do projeto
+
+```
---
+++ init.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+
+CURRENT_PATH=$(pwd)
+PREVIOUS_PATH=$(dirname "$CURRENT_PATH")
+
+POSTGRES_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+
+function initMenu() {
+    if ! dpkg-query -W -f='${Status}' dialog | grep "ok installed"; then
+        AptUpdate
+        $SUDO apt-get install -y dialog
+    fi
+
+    projectNameDialog
+
+    projectTypeDialog
+
+    setUpProject
+}
+
+function projectNameDialog() {
+    PROJECT_NAME=$(                                                             \
+                dialog                                                          \
+                    --title "Project Name"                                      \
+                    --inputbox "Digite o nome do Projeto:"                      \
+                    8 50                                                        \
+                    $PROJECT_NAME                                               \
+                    3>&1 1>&2 2>&3 3>&-
+	)
+}
+
+function projectTypeDialog() {
+    PROJECT_TYPE=$(                                                             \
+            dialog                                                              \
+                --clear                                                         \
+                --title "Tipo do projeto:"                                      \
+                --menu "Escolha o tipo do projeto que você está iniciando"      \
+                8 50 10                                                         \
+                1 "Nodejs"                                                      \
+                2 "Nodejs + Postgres"                                           \
+                3 "Nodejs + Typescript"                                         \
+                4 "Nodejs + Typescript + Postgres"                              \
+                5 "Nodejs + Typescript + Hubspot"                               \
+                2>&1 >/dev/tty
+    )
+
+    case $PROJECT_TYPE in
+        1) 
+            PROJECT_BRANCH=feature/nodejs
+            ;;
+        2) 
+            PROJECT_BRANCH=feature/nodejs-postgres
+            ;;
+        3) 
+            PROJECT_BRANCH=feature/typescript
+            ;;
+        4) 
+            PROJECT_BRANCH=feature/typescript-postgres
+            ;;
+        5) 
+            PROJECT_BRANCH=feature/typescript-hubspot
+            ;;
+    esac   
+}
+
+setUpProject() {
+    NEW_PATH="${PREVIOUS_PATH}/${PROJECT_NAME}"
+
+    cp -r $CURRENT_PATH $NEW_PATH
+    
+    cd $NEW_PATH
+
+    git checkout -f $PROJECT_BRANCH
+
+    rm -rf .git
+
+    git init
+
+    sed -i "s/PROJECT_NAME=.*/PROJECT_NAME=$PROJECT_NAME/g" $NEW_PATH/.env.example
+    sed -i "s/POSTGRES_DB=.*/POSTGRES_DB=${PROJECT_NAME}_db/g" $NEW_PATH/.env.example
+    sed -i "s/POSTGRES_USER=.*/POSTGRES_USER=docker/g" $NEW_PATH/.env.example
+    
+    cp .env.example .env
+    
+    sed -i "s/POSTGRES_PASSWORD=.*/POSTGRES_PASSWORD=$POSTGRES_PASSWORD/g" $NEW_PATH/.env
+
+    cp www/.env.example www/.env
+
+    sed -i "s/\"name\":.*/\"name\": \"${PROJECT_NAME}\",/g" $NEW_PATH/www/package.json
+
+    git add .
+
+    git commit -m "Initial Commit"
+
+    code .
+}
+
+
+initMenu
+
+clear
+
+cd $NEW_PATH
